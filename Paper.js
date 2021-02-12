29a33eaed1e4602d818c893253c0aaa8801950fe
whitehatvrashab <77061278+whitehatvrashab@users.noreply.github.com>
2021-02-12 19:33:19 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class Paper{
+ constructor(x,y,radius){
+  var options={
+    //isStatic:true,
+    restitution:0.3,
+    friction:0.5,
+    density:1.2
+    
+  }
+  this.body=Bodies.circle(x,y,radius,options)
+  this.radius=radius
+
+  World.add(world,this.body);
+ }
+
+ display(){
+    
+     push();
+    var pos =this.body.position;
+    translate(this.body.position.x, this.body.position.y);
+ fill("pink");
+
+ ellipseMode(RADIUS);
+ ellipse(0,0,10,10);
+pop();
+ }
+}
+
+ 
